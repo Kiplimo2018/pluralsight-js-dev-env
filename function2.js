ce729cc034f69a15d32685e7544db2fef805b19d
Benjamin Ngeno <bkiplimongeno@gmail.com>
2019-09-05 16:13:20 +0300

    modified the code

--- function2.js
+++ function2.js
@@ -1,6 +1,14 @@
-var firstDie = Math.ceil (6* Math.random ());
-var secondDie = Math.ceil (6* Math.random ());
+var getDieRoll = function () {
+    console.log ('rolling a die')
+    return Math.ceil (6* Math.random ());
+}
+
+var firstDie = getDieRoll ();
 
 console.log (firstDie);
+
+var secondDie = getDieRoll ();
+
 console.log (secondDie);
+
 console.log (firstDie + secondDie);
