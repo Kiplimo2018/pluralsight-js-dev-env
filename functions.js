f2df4b4981d9532a8eab85bec0efc6c1d090eed5
Benjamin Ngeno <44139499+Kiplimo2018@users.noreply.github.com>
2019-09-05 12:01:47 +0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,9 @@
+var writeGreeting = function(name) {
+    
+    var message = "Hallo " + name + ", How have been?"
+    
+    console.log (message)
+}
+
+writeGreeting ("Benjamin");
+writeGreeting ("Kiplimo");
---
+++ variablesAndtypes.js
@@ -0,0 +1,7 @@
+var x = 3, y = 2.5 , z = "3" , results = x > y;
+
+console.log ( results, x, y, z )
+console.log ( typeof y);
+console.log ( typeof z);
+console.log ( typeof console);
+console.log ( typeof results);
