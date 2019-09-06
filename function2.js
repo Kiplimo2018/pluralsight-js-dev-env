e47c2ec6769d20e5728c7f11032d0bba5ed7399c
unknown <bkiplimongeno@gmail.com>
2019-09-06 15:28:31 +0300

    I added an else if condition

--- function2.js
+++ function2.js
@@ -8,5 +8,8 @@ var roll = getDieRoll (6) ;
 if (roll >= 5 ) {
     console.log ("Great roll!");
 }
+else if (roll <= 2) {
+    console.log ( "That was a bad roll!")
+}
 
 console.log ( "You rolled a " + roll) ;
